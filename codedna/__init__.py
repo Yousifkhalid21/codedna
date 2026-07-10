9b4977e057ba328f9ec79d1dc540d5a678e20b07
Yousif Khalid Yousif Mohamed <yousifalshaier7@gmail.com>
2026-07-10 07:10:40 +0200

    Create __init__.py for CodeDNA package
    Initialize CodeDNA package with version and imports.

---
+++ codedna/__init__.py
@@ -0,0 +1,14 @@
+"""
+CodeDNA
+
+A lightweight toolkit for generating structural fingerprints
+and quality metrics for Python projects.
+"""
+
+__version__ = "0.1.0"
+
+from .analyzer import ProjectAnalyzer
+
+__all__ = [
+    "ProjectAnalyzer",
+]
